6a1669473678536ae8c6e1e9bf2a77ed8ad791a1
Vladas Cibulskis <vladas11@gmail.com>
2015-01-20 16:52:09 +0200

    v0.24

--- Sources/AssemblyInfoCommon.cs
+++ Sources/AssemblyInfoCommon.cs
@@ -22,7 +22,7 @@ using System.Reflection;
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyInformationalVersion("2.1")]
 [assembly: AssemblyFileVersion("2.0.0.24")]
-
+ 
 // AssemblyInformationalVersion should contain CI build information
 // AssemblyVersion represents marketed version
 // AssemblyFileVersion needs to change for every new WNP setup package.
