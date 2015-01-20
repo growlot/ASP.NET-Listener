4201a7acc5cb24bd32063c5573dde580561fe5e8
Vladas Cibulskis <vladas11@gmail.com>
2015-01-20 15:58:08 +0200

    LIS-86 TransactionResponse service

---
+++ Sources/Service.Implementation.KCPL/ITransactionResponse.cs
@@ -0,0 +1,56 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ITransactionResponse.cs" company="Advanced Metering Services LLC">
+//     Copyright (c) Advanced Metering Services LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AMSLLC.Listener.Service.Implementation.KCPL
+{
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.ServiceModel.Channels;
+    using AMSLLC.Listener.Service.Implementation.MessageBasedSoap;
+
+    /// <summary>
+    /// SOAP interface for transaction response service
+    /// </summary>
+    [ServiceContract(Namespace = "")]
+    [DispatchByBodyElementBehavior]
+    public interface ITransactionResponse
+    {
+        /// <summary>
+        /// Web service contract for receiving transaction responses from ODM.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        [OperationContract]
+        [DispatchBodyElement("TransactionResponseServiceRequest", "")]
+        void TransactionResponse(Message request);
+
+        /*
+         * ODM web service mocks
+         * 
+        /// <summary>
+        /// Load the asset.
+        /// </summary>
+        /// <param name="AssetLoadServiceRequest">The service request to load the asset.</param>
+        [OperationContract]
+        [DispatchBodyElement("AssetLoadServiceRequest", "")]
+        void AssetLoad(Message AssetLoadServiceRequest);
+
+        /// <summary>
+        /// Update the asset.
+        /// </summary>
+        /// <param name="AssetUpdateServiceRequest">The service request to update the asset.</param>
+        [OperationContract]
+        [DispatchBodyElement("AssetUpdateServiceRequest", "")]
+        void AssetUpdate(Message AssetUpdateServiceRequest);
+
+        /// <summary>
+        /// The test result.
+        /// </summary>
+        /// <param name="TestResultServiceRequest">The service request to save test result.</param>
+        [OperationContract]
+        [DispatchBodyElement("TestResultServiceRequest", "")]
+        void TestResult(Message TestResultServiceRequest);
+        */
+    }
+}
--- Sources/Service.Implementation.KCPL/TransactionResponseService.cs
+++ Sources/Service.Implementation.KCPL/TransactionResponseService.cs
@@ -1,18 +1,123 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-
-namespace Service.KCPL
+﻿//-----------------------------------------------------------------------
+// <copyright file="TransactionResponseService.cs" company="Advanced Metering Services LLC">
+//     Copyright (c) Advanced Metering Services LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AMSLLC.Listener.Service.Implementation.KCPL
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TransactionResponse" in both code and config file together.
+    using System;
+    using System.Configuration;
+    using System.Resources;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.ServiceModel.Channels;
+    using System.Xml;
+    using AMSLLC.Listener.Common;
+    using AMSLLC.Listener.Common.Lookup;
+    using AMSLLC.Listener.Globalization;
+    using AMSLLC.Listener.Service.Implementation.KCPL.Messages;
+    using AMSLLC.Listener.Service.Implementation.MessageBasedSoap;
+    using log4net;
+
+    /// <summary>
+    /// Implements transaction response web service.
+    /// </summary>
     public class TransactionResponseService : ITransactionResponse
     {
-        public void TransactionResponse(TransactionResponseServiceRequest reqeust)
+        /////// <summary>
+        /////// The logger
+        /////// </summary>
+        ////// private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /////// <summary>
+        /////// The string manager
+        /////// </summary>
+        ////// private ResourceManager stringManager = Init.StringManager;
+
+        /// <summary>
+        /// The transaction log manager
+        /// </summary>
+        private ITransactionManager transactionLogManager;        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransactionResponseService"/> class.
+        /// </summary>
+        public TransactionResponseService()
         {
-            throw new NotImplementedException();
+            using (IPersistenceManager persistenceManager = new PersistenceManager(ConfigurationManager.ConnectionStrings["ListenerDb"].ConnectionString))
+            {
+                IPersistenceController persistenceController = new PersistenceController();
+                persistenceController.InitializeListenerSystems(persistenceManager);
+                this.transactionLogManager = new TransactionManager(persistenceController);
+            }
+        }  
+        
+        /// <summary>
+        /// Web service contract for receiving transaction responses from ODM.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        public void TransactionResponse(Message request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            using (XmlDictionaryReader reader = request.GetReaderAtBodyContents())
+            {
+                var serializer = new DataContractSerializer(typeof(TransactionResponseServiceRequest));
+                TransactionResponseServiceRequest transactionResponse = (TransactionResponseServiceRequest)serializer.ReadObject(reader);
+
+                int transactionId;
+                
+                if (!int.TryParse(transactionResponse.listenerTransactionId, out transactionId))
+                {
+                    throw new InvalidOperationException("Can not parse listener transaction identifier to integer.");
+                }
+
+                if (transactionResponse.status == "OK")
+                {
+                    this.transactionLogManager.UpdateTransactionStatus(transactionId, TransactionStatusLookup.Succeeded, transactionResponse.message, transactionResponse.debugInfo);
+                }
+                else 
+                {
+                    // Log.Error("Transaction failed in ODM with message.")
+                    this.transactionLogManager.UpdateTransactionStatus(transactionId, TransactionStatusLookup.Failed, transactionResponse.message, transactionResponse.debugInfo);
+                }
+            }
+
+            return;
         }
+
+        /*
+         * ODM web service mocks
+         * 
+        /// <summary>
+        /// Update the asset.
+        /// </summary>
+        /// <param name="AssetUpdateServiceRequest">The service request to update the asset.</param>
+        public void AssetUpdate(Message AssetUpdateServiceRequest)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Load the asset.
+        /// </summary>
+        /// <param name="AssetLoadServiceRequest">The service request to load the asset.</param>
+        public void AssetLoad(Message AssetLoadServiceRequest)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// The test result.
+        /// </summary>
+        /// <param name="TestResultServiceRequest">The service request to save test result.</param>
+        public void TestResult(Message TestResultServiceRequest)
+        {
+            return;
+        }
+        */
     }
 }
