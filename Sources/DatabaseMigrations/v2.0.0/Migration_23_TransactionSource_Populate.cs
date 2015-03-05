375d23202d6a2fb3c0c06cbe1b45c1e8a7327467
Vladas Cibulskis <vladas11@gmail.com>
2015-03-05 09:26:14 +0200

    LIS-149 Fixing speling error in DB transaction source "Web Service Call"

---
+++ Sources/DatabaseMigrations/v2.0.0/Migration_23_TransactionSource_Populate.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Migration_23_TransactionSource_Populate.cs" company="Advanced Metering Services LLC">
+//     Copyright (c) Advanced Metering Services LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AMSLLC.Listener.DatabaseMigrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FluentMigrator;
+
+    /// <summary>
+    /// Performs a database migration
+    /// </summary>
+    [Migration(23)]
+    public class Migration_23_TransactionSource_Populate : Migration
+    {
+        /// <summary>
+        /// Performs the database migration
+        /// </summary>
+        public override void Up()
+        {
+            Update.Table("TransactionSource").Set(new { Description = "Web Service Call" }).Where(new { TransactionSourceId = "1" });
+        }
+
+        /// <summary>
+        /// Rolls back the database migration
+        /// </summary>
+        public override void Down()
+        {
+        }
+    }
+}
