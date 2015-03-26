2b38177871dd35e5f16f20a01384cc1416c1757d
Vladas Cibulskis <vladas11@gmail.com>
2015-03-26 17:57:14 +0200

    v0.28

--- Sources/AssemblyInfoCommon.cs
+++ Sources/AssemblyInfoCommon.cs
@@ -21,7 +21,7 @@ using System.Reflection;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyInformationalVersion("2.1")]
-[assembly: AssemblyFileVersion("2.0.0.27")]
+[assembly: AssemblyFileVersion("2.0.0.28")]
  
 // AssemblyInformationalVersion should contain CI build information
 // AssemblyVersion represents marketed version
