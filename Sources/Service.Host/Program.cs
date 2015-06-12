85c8c0c2e3d90ab01c76db8295a05ef57d3c8764
Vladas Cibulskis <vladas11@gmail.com>
2015-06-12 13:08:35 +0300

    Fixing Disposable field issue and minor code cleanup.

--- Sources/Service.Host/Program.cs
+++ Sources/Service.Host/Program.cs
@@ -20,18 +20,12 @@ namespace AMSLLC.Listener.Service.Host
     /// <summary>
     /// Main entry class for the application.
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable", Justification = "temp")]
     public class Program
     {
         /// <summary>
         /// The logger
         /// </summary>
         private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
-        /// <summary>
-        /// The composition container
-        /// </summary>
-        private CompositionContainer container;
 
         /// <summary>
         /// The service host
@@ -42,9 +36,10 @@ namespace AMSLLC.Listener.Service.Host
 #pragma warning restore 0649
 
         /// <summary>
-        /// Prevents a default instance of the <see cref="Program"/> class from being created.
+        /// Creates non static instance of the <see cref="Program"/> class.
+        /// <see href="https://msdn.microsoft.com/en-us/library/dd460648(v=vs.100).aspx">MEF reference</see>
         /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "testing")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "Need to catch all exceptions before pgoram crashes, so they would be logged")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "System.Console.WriteLine(System.String)", Justification = "This method is used only for debugging of program and not presented to user")]
         private Program()
         {
@@ -53,47 +48,48 @@ namespace AMSLLC.Listener.Service.Host
                 catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
                 catalog.Catalogs.Add(new DirectoryCatalog(Environment.CurrentDirectory + "\\Plugins"));
 
-                this.container = new CompositionContainer(catalog);
-
-                try
+                using (CompositionContainer container = new CompositionContainer(catalog))
                 {
-                    this.container.ComposeParts(this);
-                }
-                catch (Exception exception)
-                {
-                    Logger.Error("Composition exception occured.", exception);
-                    Console.WriteLine(exception.ToString());
-                }
-
-                try
-                {
-                    if (!Environment.UserInteractive)
+                    try
                     {
-                        ServiceBase[] servicesToRun;
-                        servicesToRun = new ServiceBase[] 
-                            { 
-                                this.serviceHost
-                            };
-                        ServiceBase.Run(servicesToRun);
+                        container.ComposeParts(this);
                     }
-                    else
+                    catch (CompositionException exception)
                     {
-                        this.serviceHost.OpenAll();
+                        Logger.Error("Composition exception occured.", exception);
+                        Console.WriteLine(exception.ToString());
+                    }
+
+                    try
+                    {
+                        if (!Environment.UserInteractive)
+                        {
+                            ServiceBase[] servicesToRun;
+                            servicesToRun = new ServiceBase[] 
+                                { 
+                                    this.serviceHost
+                                };
+                            ServiceBase.Run(servicesToRun);
+                        }
+                        else
+                        {
+                            this.serviceHost.OpenAll();
+                            Console.WriteLine("**");
+                            Console.WriteLine("** Web service started successfully. Press any key to stop.");
+                            Console.WriteLine("**");
+                            Console.ReadKey(true);
+
+                            this.serviceHost.CloseAll();
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        Logger.Error("Unknown error occured.", exception);
                         Console.WriteLine("**");
-                        Console.WriteLine("** Web service started successfully. Press any key to stop.");
+                        Console.WriteLine("** Web service failed to start. Press any key to exit.");
                         Console.WriteLine("**");
                         Console.ReadKey(true);
-
-                        this.serviceHost.CloseAll();
                     }
-                }
-                catch (Exception exception)
-                {
-                    Logger.Error("Unknown error occured.", exception);
-                    Console.WriteLine("**");
-                    Console.WriteLine("** Web service failed to start. Press any key to exit.");
-                    Console.WriteLine("**");
-                    Console.ReadKey(true);
                 }
             }
         }
@@ -102,11 +98,11 @@ namespace AMSLLC.Listener.Service.Host
         /// The main entry point for the application.
         /// Runs service as a console application or as windows service.
         /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "program", Justification = "Need to initialize the composition of the program")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", MessageId = "AMSLLC.Listener.Service.Host.Program", Justification = "Call to constructor actually runs the whole program.")]
         public static void Main()
         {
             XmlConfigurator.Configure();
-            Program program = new Program();
+            new Program();
         }
     }
 }
