96854659140bf5175bf625ee5a0bef934dcb172f
Vladas Cibulskis <vladas11@gmail.com>
2015-08-05 19:10:30 +0300

    Migration to VS 2015 (switching to StyleCop.Analyzers)

--- Sources/AssemblyInfoCommon.cs
+++ Sources/AssemblyInfoCommon.cs
@@ -12,17 +12,17 @@ using System.Reflection;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.2.0.0")]
 [assembly: AssemblyInformationalVersion("2.2")]
 [assembly: AssemblyFileVersion("2.2.0.14")]
- 
+
 // AssemblyInformationalVersion should contain CI build information
 // AssemblyVersion represents marketed version
 // AssemblyFileVersion needs to change for every new WNP setup package.
