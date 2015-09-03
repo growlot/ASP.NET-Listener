d476d2cd6e1cc3a983da2500a2fa6192840bea35
Vladas Cibulskis <vladas11@gmail.com>
2015-09-03 11:33:20 +0300

    LIS-281 Migrated current domain implementation to new project

---
+++ Sources/WNP.Listener/Domain/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IUnitOfWork.cs" company="Advanced Metering Services LLC">
+//     Copyright (c) Advanced Metering Services LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AMSLLC.Listener.Domain
+{
+    /// <summary>
+    /// Interface for managing unit of work.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Commits this unit of work.
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// Rollbacks this unit of work.
+        /// </summary>
+        void Rollback();
+    }
+}
