bc779e7d82dafe19b8cd61ba2265ea24a47d25d1
Vladas Cibulskis <vladas11@gmail.com>
2015-10-19 09:47:50 +0300

    Adding missing files to fix build.

---
+++ Sources/WNP.Listener/ApplicationService/SummaryBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace AMSLLC.Listener.ApplicationService
+{
+    using System;
+    using System.Collections.Generic;
+    using Domain.Listener.Transaction;
+
+    public class SummaryBuilder : ISummaryBuilder
+    {
+        public void Build(string data, Dictionary<string, object> summary, IEnumerable<FieldConfiguration> fieldConfigurations)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sources/WNP.Listener/Domain.Listener/Transaction/ISummaryBuilder.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ISummaryBuilder.cs" company="Advanced Metering Services LLC">
+//     Copyright (c) Advanced Metering Services LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AMSLLC.Listener.Domain.Listener.Transaction
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interface for building summary.
+    /// </summary>
+    public interface ISummaryBuilder
+    {
+        /// <summary>
+        /// Builds the summary.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="summary">The summary.</param>
+        /// <param name="fieldConfigurations">The field configurations.</param>
+        void Build(string data, Dictionary<string, object> summary, IEnumerable<FieldConfiguration> fieldConfigurations);
+    }
+}
