68a07934a1b4a2f7b9b040552f8caa4c984c4bb3
Audrius Seskas <audriusseskas@gmail.com>
2015-10-23 08:10:32 -0400

    Summary builder

--- Sources/WNP.Listener/ApplicationService/SummaryBuilder.cs
+++ Sources/WNP.Listener/ApplicationService/SummaryBuilder.cs
@@ -1,14 +1,39 @@
-﻿namespace AMSLLC.Listener.ApplicationService
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMSLLC.Listener.ApplicationService
 {
-    using System;
-    using System.Collections.Generic;
+    using System.Dynamic;
     using Domain.Listener.Transaction;
+    using Newtonsoft.Json;
 
     public class SummaryBuilder : ISummaryBuilder
     {
-        public void Build(string data, Dictionary<string, object> summary, IEnumerable<FieldConfiguration> fieldConfigurations)
+        public void Build(object data, Dictionary<string, object> summary, IEnumerable<FieldConfiguration> fieldConfigurations)
         {
-            throw new NotImplementedException();
+            object workData = data;
+            var value = data as string;
+            if (value != null)
+            {
+                workData = JsonConvert.DeserializeObject<ExpandoObject>(value);
+            }
+
+
+            foreach (var fieldConfiguration in fieldConfigurations)
+            {
+                DynamicUtilities.ProcessProperty(workData, fieldConfiguration.Name, null,
+                    (targetProperty, owner, propRef) =>
+                    {
+                        var targetValue = targetProperty.GetValue(owner);
+                        if (fieldConfiguration.HashSequence.HasValue)
+                        {
+                            summary.Add(fieldConfiguration.Name, targetValue);
+                        }
+                    });
+            }
         }
     }
 }
--- Sources/WNP.Listener/Domain.Listener/Transaction/ISummaryBuilder.cs
+++ Sources/WNP.Listener/Domain.Listener/Transaction/ISummaryBuilder.cs
@@ -1,23 +1,24 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="ISummaryBuilder.cs" company="Advanced Metering Services LLC">
-//     Copyright (c) Advanced Metering Services LLC. All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
+﻿// //-----------------------------------------------------------------------
+// // <copyright file="ISummaryBuilder.cs" company="Advanced Metering Services LLC">
+// //     Copyright (c) Advanced Metering Services LLC. All rights reserved.
+// // </copyright>
+// //-----------------------------------------------------------------------
+
 namespace AMSLLC.Listener.Domain.Listener.Transaction
 {
     using System.Collections.Generic;
 
     /// <summary>
-    /// Interface for building summary.
+    /// Summary builder interface
     /// </summary>
     public interface ISummaryBuilder
     {
         /// <summary>
-        /// Builds the summary.
+        /// Builds the specified summary.
         /// </summary>
         /// <param name="data">The data.</param>
         /// <param name="summary">The summary.</param>
         /// <param name="fieldConfigurations">The field configurations.</param>
-        void Build(string data, Dictionary<string, object> summary, IEnumerable<FieldConfiguration> fieldConfigurations);
+        void Build(object data, Dictionary<string, object> summary, IEnumerable<FieldConfiguration> fieldConfigurations);
     }
 }
