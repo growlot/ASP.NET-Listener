5722ccb526f6d3ca360f9bef806e1f4f2fb113aa
Audrius Seskas <audriusseskas@gmail.com>
2015-12-17 06:01:30 -0500

    Exposing value map entries

---
+++ Sources/WNP.Listener/Persistence.Listener/Models/ValueMapEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace AMSLLC.Listener.Persistence.Listener
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [MetadataType(typeof(ValueMapMetadata))]
+    partial class ValueMapEntity
+    {
+        public virtual ICollection<ValueMapEntryEntity> ValueMapEntries { get; set; } = new Collection<ValueMapEntryEntity>();
+
+        internal sealed class ValueMapMetadata
+        {
+        }
+    }
+
+}
