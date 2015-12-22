f220f9adf696b2b9337d5564b9965f212dff63d5
Audrius Seskas <audriusseskas@gmail.com>
2015-12-22 06:09:39 -0500

    Controller updated with CRUD operations

---
+++ Sources/WNP.Listener/ODataService/Controllers/ValueMapController.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="ValueMapController.cs" company="Advanced Metering Services LLC">
+//     Copyright (c) Advanced Metering Services LLC. All rights reserved.
+// </copyright>
+
+namespace AMSLLC.Listener.ODataService.Controllers
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using Persistence.Listener;
+
+    partial class ValueMapController
+    {
+        partial void UpdateNested(
+            ValueMapEntity entity,
+            ValueMapEntity newData)
+        {
+            var toDelete =
+                entity.ValueMapEntries.Where(
+                    s => newData.ValueMapEntries.All(ss => ss.ValueMapEntryId != s.ValueMapEntryId)).ToList();
+
+            var toUpdate =
+                entity.ValueMapEntries.Where(
+                    s => newData.ValueMapEntries.Any(ss => ss.ValueMapEntryId == s.ValueMapEntryId)).ToList();
+
+            foreach (ValueMapEntryEntity valueMapEntryEntity in toDelete)
+            {
+                this._dbContext.Entry(valueMapEntryEntity).State = EntityState.Deleted;
+            }
+
+            foreach (ValueMapEntryEntity valueMapEntryEntity in entity.ValueMapEntries)
+            {
+                var newValues =
+                    newData.ValueMapEntries.SingleOrDefault(
+                        s => s.ValueMapEntryId == valueMapEntryEntity.ValueMapEntryId);
+                if (newValues != null)
+                {
+                    this._dbContext.Entry(valueMapEntryEntity).CurrentValues.SetValues(newValues);
+                }
+
+            }
+
+            foreach (ValueMapEntryEntity valueMapEntryEntity in newData.ValueMapEntries)
+            {
+                if (valueMapEntryEntity.ValueMapEntryId <= 0)
+                {
+                    this._dbContext.Entry(valueMapEntryEntity).State = EntityState.Added;
+                }
+            }
+        }
+    }
+}
