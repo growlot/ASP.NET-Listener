646b403a91ce10b9089dfceaee58b142c7e30fcb
Audrius Seskas <audriusseskas@gmail.com>
2015-12-27 12:07:51 -0500

    Updated with additional operations

---
+++ Sources/WNP.Listener/ODataService/Controllers/FieldConfigurationController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMSLLC.Listener.ODataService.Controllers
+{
+    using System.Data.Entity;
+    using Persistence.Listener;
+
+    partial class FieldConfigurationController
+    {
+        partial void UpdateNested(
+            FieldConfigurationEntity entity,
+            FieldConfigurationEntity newData)
+        {
+            var toDelete =
+                entity.FieldConfigurationEntries.Where(
+                    s => newData.FieldConfigurationEntries.All(ss => ss.FieldConfigurationEntryId != s.FieldConfigurationEntryId)).ToList();
+
+            var toUpdate =
+                entity.FieldConfigurationEntries.Where(
+                    s => newData.FieldConfigurationEntries.Any(ss => ss.FieldConfigurationEntryId == s.FieldConfigurationEntryId)).ToList();
+
+            foreach (FieldConfigurationEntryEntity entry in toDelete)
+            {
+                this._dbContext.Entry(entry).State = EntityState.Deleted;
+            }
+
+            foreach (FieldConfigurationEntryEntity entry in entity.FieldConfigurationEntries)
+            {
+                var newValues =
+                    newData.FieldConfigurationEntries.SingleOrDefault(
+                        s => s.FieldConfigurationEntryId == entry.FieldConfigurationEntryId);
+                if (newValues != null)
+                {
+                    this._dbContext.Entry(entry).CurrentValues.SetValues(newValues);
+                }
+
+            }
+
+            foreach (FieldConfigurationEntryEntity entry in newData.FieldConfigurationEntries)
+            {
+                if (entry.FieldConfigurationEntryId <= 0)
+                {
+                    this._dbContext.Entry(entry).State = EntityState.Added;
+                }
+            }
+        }
+    }
+}
---
+++ Sources/WNP.Listener/Persistence.Listener/Models/FieldConfigurationEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMSLLC.Listener.Persistence.Listener
+{
+    using System.Collections.ObjectModel;
+
+    public partial class FieldConfigurationEntity
+    {
+        public virtual ICollection<FieldConfigurationEntryEntity> FieldConfigurationEntries { get; set; } = new Collection<FieldConfigurationEntryEntity>();
+    }
+}
